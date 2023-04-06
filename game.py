c6be0b0890f2920092f53c68d59bec6b177f214f
Ольга Касапенко <kasapenko.oa@phystech.edu>
2023-04-06 12:51:04 +0300

    game

---
+++ game.py
@@ -0,0 +1,57 @@
+import pygame
+import sys
+from bird_part import bird
+from cloud_part import Cloud
+import button
+from pygame import sprite
+from pygame.sprite import Group
+import time
+
+def run():
+  pygame.init()
+  screen = pygame.display.set_mode((500, 500))
+  pygame.display.set_caption("Игра")
+  pygame.mixer.music.load("birds_song.mp3")
+  black_color = (0, 0, 0)
+  endgame_color = (200, 200, 200)
+  bird_1 = bird(screen)
+  cloud_1 = Group()
+  number_of_clouds = 0
+  count = 0
+  game_over_pic = pygame.image.load('game_over.png')
+  pygame.mixer.music.play(-1)
+
+  while True:
+    #добавление облаков
+    if number_of_clouds == 0:
+      new_cloud = Cloud(screen)
+      cloud_1.add(new_cloud)
+      number_of_clouds += 1
+      count += 1
+    #управление птицей
+    button.buttons(bird_1)
+    bird_1.bird_position()
+    screen.fill(black_color)
+    for cl in cloud_1.sprites():
+      cl.drawcl()
+    bird_1.output()
+    cloud_1.update()
+    #удаление облаков после выхода за экран
+    for cl in cloud_1.copy():
+      if cl.rect.right <= screen.get_rect().left:
+        cloud_1.remove(cl)
+        number_of_clouds -= 1
+    for cl in cloud_1.copy():
+      if bird_1.rect.right <= cl.rect.right and bird_1.rect.left >= cl.rect.left and bird_1.rect.top <= cl.rect.bottom and bird_1.rect.bottom >= cl.rect.top:
+        data_file = open("data.txt", "a")
+        data_file.write(str(count) + '\n')
+        data_file.close()
+        pygame.mixer.music.load("game_over_sound.mp3")
+        pygame.mixer.music.play(0)
+        screen.blit(game_over_pic, (0, 0))
+        pygame.display.flip()
+        time.sleep(3)
+        sys.exit()
+    pygame.display.flip()
+
+run()
