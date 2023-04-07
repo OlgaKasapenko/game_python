718dae4c8672ae87748d75cc17b275993489fba7
Ольга Касапенко <kasapenko.oa@phystech.edu>
2023-04-07 19:02:42 +0300

    Records

--- game.py
+++ game.py
@@ -44,7 +44,7 @@ def run():
     for cl in cloud_1.copy():
       if bird_1.rect.right <= cl.rect.right and bird_1.rect.left >= cl.rect.left and bird_1.rect.top <= cl.rect.bottom and bird_1.rect.bottom >= cl.rect.top:
         data_file = open("data.txt", "a")
-        data_file.write(str(user_name.get_value()) + ' ' + str(count) + '\n')
+        data_file.write(str(str(count) + ' ' + user_name.get_value()) + '\n')
         data_file.close()
         pygame.mixer.music.load("game_over_sound.mp3")
         pygame.mixer.music.play(0)
@@ -54,14 +54,50 @@ def run():
         sys.exit()
     pygame.display.flip()
 
+def sort_key(line):
+    parts = line.split()
+    return int(parts[0])
 
-pygame.init()
-screen0 = pygame.display.set_mode((500, 500))
-pygame.display.set_caption("Игра")
-screen0.fill((100, 100, 100))
-menu = pygame_menu.Menu('хотите сыграть?', 500, 500,
-                      theme=pygame_menu.themes.THEME_DARK)
-user_name = menu.add.text_input('Имя :', default='Маша')
-menu.add.button('Играть!', run)
-menu.add.button('Выйти', pygame_menu.events.EXIT)
-menu.mainloop(screen0)
+def sort_lines(filename):
+  with open(filename, 'r') as f:
+      lines = f.readlines()
+  lines = sorted(lines, key=sort_key, reverse=True)
+  with open(filename, 'w') as f:
+      f.writelines(lines)
+
+def read_beg_of_file(filename):
+  with open(filename, 'r') as f:
+    text = ''
+    for i in range(3):
+      text += f.readline()
+  return text
+
+def record_t():
+  pygame.mixer.music.load("джентельмены_удачи.mp3")
+  pygame.mixer.music.play(-1)
+  pygame.init()
+  screen2 = pygame.display.set_mode((500, 500))
+  pygame.display.set_caption("Рекорды")
+  menu2 = pygame_menu.Menu('рекорды', 500, 500, theme=pygame_menu.themes.THEME_DARK)
+  sort_lines('data.txt')
+  text = read_beg_of_file('data.txt')
+  menu2.add.label(text)
+  menu2.add.button('Назад', menu)
+  menu2.mainloop(screen2)
+
+
+def menu():
+  pygame.init()
+  pygame.mixer.music.load("menu_sound.mp3")
+  pygame.mixer.music.play(-1)
+  screen0 = pygame.display.set_mode((500, 500))
+  pygame.display.set_caption("Игра")
+  menu = pygame_menu.Menu('хотите сыграть?', 500, 500, theme=pygame_menu.themes.THEME_DARK)
+  global user_name
+  user_name = menu.add.text_input('Имя :', default='Маша')
+  menu.add.button('Играть!', run)
+  menu.add.button('Таблица рекордов', record_t)
+  menu.add.button('Выйти', pygame_menu.events.EXIT)
+  menu.mainloop(screen0)
+
+menu()
